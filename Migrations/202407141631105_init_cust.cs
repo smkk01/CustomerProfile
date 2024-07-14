f397d2f6ba7927967038d44892a9fcd00818567b
smkk01 <sumonkk@gmail.com>
2024-07-15 01:02:39 +0800

    Add files via upload

---
+++ Migrations/202407141631105_init_cust.cs
@@ -0,0 +1,32 @@
+﻿namespace MVCwithWebAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init_cust : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Age = c.String(),
+                        DOB = c.String(),
+                        Gender = c.String(),
+                        Occupation = c.String(),
+                        Email = c.String(),
+                        PhoneNo = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
